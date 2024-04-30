24973e27a3266295b736370356e7dda2c4960395
VaibhavKumbhar75 <131513816+VaibhavKumbhar75@users.noreply.github.com>
2024-04-30 12:37:08 +0530

    Welcome Msg

--- src/main/java/org/example/MoodAnalyzer.java
+++ src/main/java/org/example/MoodAnalyzer.java
@@ -1,6 +1,7 @@
 package org.example;
 
-public class Main {
+public class MoodAnalyzer {
+
     public static void main(String[] args) {
 
         System.out.println("Welcome to Mood Analyzer Problem");
