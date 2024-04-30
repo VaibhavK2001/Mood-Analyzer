b1227023fb532795e5360cbe5adec410944f06df
VaibhavKumbhar75 <131513816+VaibhavKumbhar75@users.noreply.github.com>
2024-04-30 12:42:25 +0530

    Mood Analyzer Object

--- src/main/java/org/example/MoodAnalyzer.java
+++ src/main/java/org/example/MoodAnalyzer.java
@@ -2,8 +2,21 @@ package org.example;
 
 public class MoodAnalyzer {
 
+    public String analyzeMood(String mood){
+        if (mood.equalsIgnoreCase("Happy")){
+            return "HAPPY";
+        }if (mood.equalsIgnoreCase("Sad")){
+            return "SAD";
+        }else{
+            return null;
+        }
+    }
+
     public static void main(String[] args) {
 
         System.out.println("Welcome to Mood Analyzer Problem");
+        MoodAnalyzer moodAnalyzer = new MoodAnalyzer();
+        String mood = moodAnalyzer.analyzeMood("happy");
+        System.out.println(mood);
     }
 }
