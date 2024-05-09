e4504218735759381496cde7fa1bda8b65d8ba01
VaibhavKumbhar75 <131513816+VaibhavKumbhar75@users.noreply.github.com>
2024-05-09 12:47:45 +0530

    Mood Analysis Exception for Null

--- src/test/java/org/example/MoodAnalyzerTest.java
+++ src/test/java/org/example/MoodAnalyzerTest.java
@@ -5,5 +5,12 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class MoodAnalyzerTest {
-    
+
+    @Test
+    public void MoodAnalysisExceptionTest() throws MoodAnalysisException{
+
+        MoodAnalyzer moodAnalyzer = new MoodAnalyzer(null);
+        String mood = moodAnalyzer.analyzeMood();
+        Assertions.assertEquals("HAPPY",mood);
+    }
 }
